3cc234f8dea22fa5853e33db253d70e3f2fef93e
manueltov <manuel@tovar.pt>
2019-12-03 15:49:34 +0000

    add super pastilha

--- projIIA_22.py
+++ projIIA_22.py
@@ -10,7 +10,10 @@ def pac_22(gState,player):
     #distancia ah pastilha mais proxima
     c = distAhPastilhaMaisProxima(gState,player)
 
-    return (a * 1) + (b * 1) + (c * 1)
+    #distancia ah super pastilha mais proxima
+    d = distAhSuperPastilhaMaisProxima(gState,player)
+
+    return (a * 1) + (b * 1) + (c * 1) + (d * 1)
 
 "esta eh a funcao que fara o fantama pensar em como maximizar o score para si"
 def fant_22(gState,player):
@@ -23,6 +26,8 @@ def extraP_22() :
 "esta funcao que o prof pede eh um extra relativamente ao fantasma"
 def extraF_22() :
     return 0
+
+
 
 ##############################
 ######   UTILIDADES   ########
@@ -42,6 +47,19 @@ def distAhPastilhaMaisProxima(gState,player):
         minDistance = min([manhatanDist(myPos, food) for food in foodList])
     return minDistance
 
+"""
+-> calcula a distancia ah super pastilha mais proxima
+-> atualmente usa o manhattanDistance
+-> ista funcao eh quase uma copia da funcao distAhPastilhaMaisProxima()
+"""
+def distAhSuperPastilhaMaisProxima(gState,player):
+    superFoodList = gState.board.getCapsules()
+    minDistance = 0
+    # Compute distance to the nearest food
+    if len(superFoodList) > 0: # This should always be True,  but better safe than sorry
+        myPos = gState.board.getPacmanPosition()
+        minDistance = min([manhatanDist(myPos, superFood) for superFood in superFoodList])
+    return minDistance
 
 """
 num futuro podemos ter aqui uma melhor que o manhattanDistance
@@ -52,6 +70,7 @@ def manhatanDist(p1,p2):
     p1x,p1y=p1
     p2x,p2y=p2
     return abs(p1x-p2x)+abs(p1y-p2y)
+
 
 
 ####################################################
