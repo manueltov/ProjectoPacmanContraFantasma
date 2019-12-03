6b909531103b4adaaf370373a46dee8793c2434b
manueltov <manuel@tovar.pt>
2019-12-03 17:28:29 +0000

    trying to put parameter generator

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ agent_args = "depth=8,evalFn=projIIA_22.pac_22"
 ghost_args = "depth=8,evalFn=anti_score"
 
 def runPacmanMain(opt_ghost_args = ghost_args, opt_agent_args = agent_args):
-    args = pacman.readCommand(["-g", "AlphaBetaGhost", "-b", opt_ghost_args, "-p", "AlphaBetaAgent", "-a", opt_agent_args, "--frameTime", "0", "-k", "1", "-l", "mediumClassic"] )
+    args = pacman.readCommand(["-n", "2", "-q", "-g", "AlphaBetaGhost", "-b", opt_ghost_args, "-p", "AlphaBetaAgent", "-a", opt_agent_args, "--frameTime", "0", "-k", "1", "-l", "mediumClassic"] )
     pacman.runGames(**args)
 
 runPacmanMain()
